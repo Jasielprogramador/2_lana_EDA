47e95ebc5ca22502af2148510b7a4cef51b31e0d
Asi <asierrosa@gmail.com>
2020-10-21 10:46:27 +0200

    interfazeen metodoen inplementazio batzuk

--- src/proiektua/OrderedCircularLinkedList.java
+++ src/proiektua/OrderedCircularLinkedList.java
@@ -2,7 +2,7 @@ package proiektua;
 
 
 import java.util.LinkedList;
-public class OrderedCircularLinkedList<T> extends CircularLinkedList{
+public class OrderedCircularLinkedList<T> extends CircularLinkedList extends Comparable<T>{
 	
 	public OrderedCircularLinkedList(String d, int c) {
 		super(d,c);
@@ -34,5 +34,56 @@ public class OrderedCircularLinkedList<T> extends CircularLinkedList{
 	public void merge(OrderedCircularLinkedList<T> lista) {
 		
 	}
+	
+	
+	
+	public void setDeskr(String nom) {
+		
+	}
+	
+	public String getDeskr() {
+		
+	}
+	
+	public T removeFirst() {
+		
+	}
+	
+	public T removeLast(T elem) {
+		
+	}
+	
+	public T remove(T elem) {
+		 // Balio hori listan baldin badago, bere lehen agerpena ezabatuko dut. Kendutako objektuaren erreferentzia bueltatuko du (null ez baldin badago)
+	}
+	
+	public T first() {
+		
+	}
+	
+	public T last() {
+		
+	}
+	
+	public boolean contains(T elem) {
+		
+	}
+	
+	public T find(T elem) {
+		
+	}
+	
+	public boolean isEmpty() {
+		
+	}
+	
+	public int size() {
+		
+	}
+	
+	public Iterator<T> iterator(){
+		
+	}
+	
 	
 }
