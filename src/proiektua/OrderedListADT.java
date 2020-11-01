d1aad919e7d5ced34a0b5a61d82c1b8b8fb8cebe
arrosa <asierrosa@gmail.com>
2020-11-01 10:57:39 +0100

    ñljhsdefkñjasdf

--- src/proiektua/OrderedListADT.java
+++ src/proiektua/OrderedListADT.java
@@ -1,6 +1,6 @@
 package proiektua;
 
-public interface OrderedListADT<T> extends ListADT<T>{
+public interface OrderedListADT<T extends Comparable<T>> extends ListADT<T>{
 	public void add(T elem);
-	public void merge(OrderedCircularLinkedList<T> z);mugica
+	public void merge(OrderedCircularLinkedList<T> z);
 }
